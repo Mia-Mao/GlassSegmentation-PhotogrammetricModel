ba03aa1f6ebc9ac4f01cc6d9dd7dca2529f2b611
Zhu Mao <54829432+Mia-Mao@users.noreply.github.com>
2021-11-06 22:05:50 +0800

    Add files via upload

---
+++ Evaluation.py
@@ -0,0 +1,166 @@
+
+import numpy as np
+import os
+from lib.read_data.OBJ_PARSER import *
+
+
+class data_PARSER(OBJ_PARSER):
+	def __init__(self, model_file):
+		super(data_PARSER,self).__init__(model_file)
+
+	def get_triangles(self):
+		faces = self.get_faces()
+		vertices = self.get_vertices()
+		for index in faces:
+			triangle = [vertices[index[0]-1], vertices[index[2]-1], vertices[index[4]-1]]
+			triangle = np.array(triangle).astype(np.float32)
+			triangle = tuple(map(str, triangle))
+			self.triangles.append(triangle)
+		return self.triangles, faces, vertices
+
+
+def read_3D_segments(dirpath):
+	segments = {}
+	
+	obj_files = os.listdir(dirpath)
+	
+	for obj_file in obj_files:
+		
+		model_file = os.path.join(dirpath, obj_file)
+		parser = data_PARSER(model_file)
+		triangles, faces, vertices = parser.get_triangles()
+		segments[obj_file] = triangles
+		
+		# print('triangles number of %s is %d: '% (obj_file, len(triangles)))
+	return segments
+
+
+
+def evaluate(gt_segments, pred_segments, threshold = 0.5):
+	
+	pred_num = len(pred_segments.keys())
+	gt_num = len(gt_segments.keys())
+
+	print('pred_num:', pred_num)
+	print('gt_num:', gt_num)
+
+	total_precision = 0.0
+	total_recall = 0.0
+	total_iou = 0.0
+
+	for obj in pred_segments.keys():
+
+		print('----------------------------------------------')
+
+		print('obj_name:', obj)
+		pred_triangles = pred_segments[obj]
+		pred_triangles_num = len(pred_triangles)
+
+		gt_triangles = gt_segments[obj]
+		gt_triangles_num = len(gt_triangles)
+
+		print(pred_triangles_num, gt_triangles_num)
+
+		intersection = len(set(pred_triangles).intersection(set(gt_triangles)))
+		union = len(set(pred_triangles).union(set(gt_triangles)))
+
+		precision = float(intersection/pred_triangles_num)
+		recall = float(intersection/gt_triangles_num)
+		iou = float(intersection/union)
+
+		print('intersection:', intersection)
+		print('union:', union)
+		print('precision：', precision)
+		print('recall：',  recall)
+		print('iou：', iou)
+
+		total_precision += precision
+		total_recall += recall
+		total_iou += iou
+
+	mean_precision = float(total_precision/pred_num)
+	mean_recall = float(total_recall/pred_num)
+	mean_iou = float(total_iou/pred_num)
+
+	print('****************************************')
+ 
+	print('mean_precision', mean_precision)
+	print('mean_recall',  mean_recall)
+	print('mean_iou', mean_iou)
+
+
+	
+	exit(0)
+
+	flag = np.zeros(gt_num)
+	gt_names = list(gt_segments.keys())
+
+
+	# pixel_level_precision, pixel_level_recall = 0.0, 0.0
+	instance_tp = 0.0
+	mIoU = []
+	matches = []
+
+	for pred_name, pred_segs in pred_segments.items():
+
+		max_iou = 0
+		match_gt_name = 0
+		match_intersection = 0
+		match_union = 0
+		
+		for gt_name, gt_segs in gt_segments.items():
+
+			intersection = len(set(pred_segs).intersection(set(gt_segs)))
+			union = len(set(pred_segs).union(set(gt_segs)))
+
+			iou = intersection*1.0/union
+
+			if iou>max_iou:
+				max_iou = iou
+				match_gt_name = gt_name
+				match_intersection = intersection
+				match_union = union
+			
+		if max_iou>=threshold:
+			instance_tp +=1
+			ind = gt_names.index(match_gt_name)
+			flag[ind] = 1
+
+			mIoU.append(max_iou)
+			matches.append((pred_name, match_gt_name))
+
+	instance_level_precision = instance_tp*1.0/pred_num
+	instance_level_recall = np.sum(flag)*1.0 / gt_num
+	mIoU = np.array(mIoU)
+	mIoU = np.mean(mIoU)
+
+	left_pred = set(pred_segments) - set(np.unique(matches))
+	left_gt = set(gt_segments) - set(np.unique(matches))
+	if len(left_gt) and len(left_pred):
+		temp = []
+		for left_p in left_pred:
+			for left_g in left_gt:
+				intersection = len(set(left_p).intersection(set(left_g)))
+				union = len(set(left_p).union(set(left_g)))
+
+				iou = intersection*1.0/union
+				temp.append((left_g, left_p,iou))
+		temp.sort(key = lambda  k:k[-1], reverse = True)
+	
+	return instance_level_precision, instance_level_recall, mIoU, matches
+
+
+
+
+if __name__ == "__main__":
+
+	gt_dirpath = './evaluation/truth/'
+	pred_dirpath = './evaluation/predict/'
+
+	gt_segments = read_3D_segments(gt_dirpath)
+	pred_segments = read_3D_segments(pred_dirpath)
+
+	instance_level_precision, instance_level_recall, mIOU, matches = evaluate(gt_segments, pred_segments)
+	print(instance_level_precision, instance_level_recall, mIOU, matches, len(matches))
+
+
---
+++ main.py
@@ -0,0 +1,99 @@
+import _init_paths
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import colorsys,random
+from read_data.OBJ_PARSER import *
+from read_data.XML_PARSER import *
+from read_data.MASK_PARSER import *
+from projection_2D_3D.projection import *
+import pickle
+
+def generate_colors(N = 10):
+	HSV_tuples = [(x*1.0/N, 0.7, 0.7) for x in range(N)]
+	RGB_tuples = list(map(lambda x: colorsys.hsv_to_rgb(*x), HSV_tuples))
+	random.shuffle(RGB_tuples)
+	return RGB_tuples
+
+def get_oblimg_list(file_folder):
+
+	original_oblimg_list = os.listdir(file_folder)
+
+	return original_oblimg_list
+
+def main(AT_file, model_list, model_path, segments_path, pkl_save_folder, obj_save_folder):
+	if not os.path.exists(model_path):
+		raise(model_path + " is not exists, please check.")
+
+	original_oblimg_list = get_oblimg_list(segments_path)
+		
+	####Parsing AT fiel to obtain the parameters 解析内外参，获得所有倾斜影像的空三信息
+	para = parse_xml(AT_file)
+	args = para['args'] #AT info
+
+
+	for model in model_list:
+		print('\n ........................current model is: ' + model + '.............................')
+
+		model_name, model_ext = os.path.splitext(model)
+
+	####Parsing 3D model(.obj) to get coordinates of trangle mesh 读取三维坐标	
+		model_file = os.path.join(model_path, model)
+		obj_parser = OBJ_PARSER(model_file)
+		triangles, faces, vertices = obj_parser.get_triangles()
+		print('reading data done .....')
+		# print('triangles:', triangles)
+
+		#### stage 1: project 3D mesh to 2D image#############
+		print('stage 1: project 2D segments to 3D model .........')
+		projection_results = projection(triangles, original_oblimg_list, para, segments_path)
+		print('Finish projection.....\n')
+
+
+		pkl_temp_file = pkl_save_folder + model_name + '_temp.pkl'
+		
+	# 	# save the temp result 把results写进pkl，临时的result
+		with open(pkl_temp_file, 'wb') as temp_pkl_f:
+			pickle.dump(projection_results, temp_pkl_f)
+		temp_pkl_f.close()
+
+		# **************************************************************************
+		# stage2: remove the wrong correspondence
+		# 判断projection_results中是否有重叠的三角形，如果重叠三角形iou>0.5 同时，若有多个三角形对应同一像素，根据距离选出可视三角形
+
+		print('len(projection_results):', len(projection_results))
+		if len(projection_results)>=2:
+			filter_projection_results = tri_filter_by_distance(projection_results, triangles, para)
+		else:
+			filter_projection_results = projection_results
+
+		pkl_final_result = pkl_save_folder + model_name + '_final.pkl'
+		with open(pkl_final_result, 'wb') as pkl_final:
+			pickle.dump(filter_projection_results, pkl_final)
+		pkl_final.close()
+
+		obj_save_dir =  obj_save_folder + model_name + '_segmentResult.obj'
+
+
+		#save the final glass segmentation result in .obj file. 
+		write_into_obj(filter_projection_results, obj_save_dir, triangles)
+
+
+if __name__ == "__main__":
+	
+	model_path = '3D Models/demo/'  # 3D model folder
+	model_list = ['3D-Model-demo.obj'] # 3D model file name
+	AT_file = '3D Models/demo/AT file/AT-demo.xml' # AT_file of 3D model
+	segments_path = 'obliqueimage_detection_result/' # path for glass detection results in oblique image.
+
+	# folder to save the results.
+	pkl_save_folder = './pkl_result/demo/'
+	obj_save_folder = './obj_result/demo/'
+
+	if not os.path.exists(obj_save_folder):
+		os.makedirs(obj_save_folder)
+
+	if not os.path.exists(pkl_save_folder):
+		os.makedirs(pkl_save_folder)
+
+	main(AT_file, model_list, model_path, segments_path, pkl_save_folder, obj_save_folder) 
